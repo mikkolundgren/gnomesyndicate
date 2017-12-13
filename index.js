0fec305ad75ea95c7748775f1bdf459ed4582df4
mikko <mikko.lundgren@gmail.com>
2017-12-13 15:05:57 +0200

    basic structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+var options = {
+  dotfiles: 'ignore',
+  extensions: ['htm', 'html'],
+  index: false
+};
+
+app.use(express.static(path.join(__dirname, 'public'), options));
+
+app.get('/', function(req, res) {
+  res.render('main');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Started on port ' + app.get('port'));
+});
