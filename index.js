7426e39c6c27d5ff54ad6955ff1d5078428c49ba
mikko <mikko.lundgren@gmail.com>
2019-07-10 13:12:14 +0300

    removed cypress

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -14,11 +17,28 @@ var options = {
   index: false
 };
 
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world' };
+
 app.use(express.static(path.join(__dirname, 'public'), options));
 
 app.get('/', function(req, res) {
   res.render('main');
 });
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
 
 app.listen(app.get('port'), function() {
   console.log('Started on port ' + app.get('port'));
